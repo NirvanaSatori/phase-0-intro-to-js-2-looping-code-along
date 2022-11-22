bde0098f5e8fb2211161f16b81eb0981a22e2775
MORRIS <morris24kariuki@gmail.com>
2022-11-22 03:39:14 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const cards = ["John", "Jane", "Mary", "Joseph"];
+
+function writeCards () {
+    for ( let i = 0; i < cards.length; i++){
+    console.log (`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    
+    }
+    return [...cards];
+
+};
+const sentCards = writeCards(cards);
+
+function countDown() {
+    let count = 11;
+    while (count > 0){
+    count--;
+    console.log(count);
+}
+};
+
